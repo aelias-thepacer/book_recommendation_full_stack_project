1b7def7d3e7d0c0493e10858a0698b43aabcc0c8
aelias-thepacer <aelias.thepacer@gmail.com>
2025-02-11 19:21:19 -0700

    patching render port

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -10,7 +10,7 @@ const sequelize = process.env.DB_URL
     process.env.DB_USER || '',
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: '0.0.0.0',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
